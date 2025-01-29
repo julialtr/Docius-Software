8d277ac7f6c1133020b2b286c4d85867d0fd1a15
julialtr <julialtrapp@hotmail.com>
2025-01-29 17:20:00 -0300

    Adicionado arquivo de valores padrões dentro do banco de dados.

---
+++ Banco de Dados/db-ein-biss/EinBissInitialValues.sql
@@ -0,0 +1,20 @@
+insert into fornecedores values (1, 'Fort Atacadista', '', 'https://www.deliveryfort.com.br/');
+insert into fornecedores values (2, 'Giassi', '', 'https://www.giassi.com.br/');
+
+insert into status_pedidos values (1, 'Pagamento Pendente');
+insert into status_pedidos values (2, 'Confirmado');
+insert into status_pedidos values (3, 'Em Produção');
+insert into status_pedidos values (4, 'Concluído');
+
+insert into status_pedidos_produtos values (1, 'Concluído');
+insert into status_pedidos_produtos values (2, 'Não Concluído');
+
+insert into tipos_usuarios values (1, 'Cliente');
+insert into tipos_usuarios values (2, 'Confeiteiro');
+
+insert into unidades_medidas values (1, 'g');
+insert into unidades_medidas values (2, 'kg');
+insert into unidades_medidas values (3, 'ml');
+insert into unidades_medidas values (4, 'l');
+
+insert into usuarios values (1, 'julia', 'julia123', 'Julia Letícia Trapp', 'julialtrapp@hotmail.com', 2);
